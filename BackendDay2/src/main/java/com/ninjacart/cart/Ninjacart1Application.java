ea1e81449e374928cef21d0832426bdd0a0d3964
ADITYA KESAVAN <110535136+Aditya2160p@users.noreply.github.com>
2024-08-24 16:08:38 +0530

    Add files via upload

---
+++ BackendDay2/src/main/java/com/ninjacart/cart/Ninjacart1Application.java
@@ -0,0 +1,13 @@
+package com.ninjacart.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ninjacart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ninjacart1Application.class, args);
+	}
+
+}
